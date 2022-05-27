06acc8f9e88b33395af860d755d42ef16203755a
vitor <vitorsbsilva@gmail.com>
2022-05-27 12:59:03 -0300

    feat: create Fighter interface

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+
+interface Fighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: EnergyType;
+
+  attack(enemy: Fighter): void;
+
+  special(enemy: Fighter): void;
+
+  levelUp(): void;
+
+  receiveDamage(attackPoints: number): void;
+
+}
+
+export default Fighter;
